b3fe6c308765acb12aeac4280ad427b1197d1ae9
Madhusmita Sen <madhusmitasen27@gmail.com>
2026-03-03 17:04:21 +0530

    fix getenv for production

--- db.php
+++ db.php
@@ -3,11 +3,11 @@
 require_once __DIR__ . '/config/env.php';
 
 $conn = new mysqli(
-    $_ENV['DB_HOST'],
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASS'],
-    $_ENV['DB_NAME'],
-    $_ENV['DB_PORT'] // <-- IMPORTANT
+    getenv()['DB_HOST'],
+    getenv()['DB_USER'],
+    getenv()['DB_PASS'],
+    getenv()['DB_NAME'],
+    getenv()['DB_PORT'] // <-- IMPORTANT
 );
 if ($conn->connect_error) {
     die(json_encode([
